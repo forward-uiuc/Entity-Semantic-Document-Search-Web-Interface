c2f220b4742e8127107ea12559d5503492f42a6b
Long Pham <ltpham3@illinois.edu>
2018-06-11 09:51:01 -0500

    minor change

--- frontend/source/components/mainPage/SearchArea/SearchArea.jsx
+++ frontend/source/components/mainPage/SearchArea/SearchArea.jsx
@@ -478,7 +478,7 @@ class SearchArea extends Component {
 								<Grid.Column width={6}>
                                     <img src={this.baseUrl + "screenshots/" + crypto.createHash('md5').update(item._source.url).digest('hex') + ".png"} style={{width:'100%'}} alt="Image not found" onError="this.onerror=null;this.src='./assets/documentation.png';"/>
 								</Grid.Column>
-                                <Grid.Column width={9}>
+                                <Grid.Column width={10}>
                                     <div key={i} className="ui card padded clustercard raised">
 										<div className="content">
 
